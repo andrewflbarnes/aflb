a9f9630b87de4b72c6fa95b59ceecadef39ea7db
andrewflbarnes <andrewflbarnes@hotmail.co.uk>
2021-05-21 17:06:52 +0100

    Migrate over to version 2 :)

--- src/aflb.js
+++ src/aflb.js
@@ -1,10 +1,24 @@
-function submitContact() {
-  var formData = $("#contact-form").serializeArray();
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById("theme-switch").onclick = () => {
+    const themeEl = document.getElementById("theme");
+    const theme = themeEl.className.toLowerCase();
+    themeEl.className = theme === "dark" ? "" : "dark";
+  }
+})
 
-  var jsonData = {}
-  formData.forEach(function(entry) {
-    jsonData[entry.name] = entry.value
-  })
+function submitContact() {
+  let formInputs = document.getElementById('contact-form').getElementsByTagName('textarea');
+  let formInputsCount = formInputs.length
+
+  const jsonData = {}
+  
+  for (let i = 0; i < formInputsCount; i++) {
+    const name = formInputs[i].attributes.name.value
+    const value = formInputs[i].value
+    console.log(`${i}: ${name}=${value}`)
+    jsonData[name] = value;
+  }
+  console.log(jsonData)
 
   var resOk = false
   var resStatus = 0
@@ -22,16 +36,17 @@ function submitContact() {
     if (!resOk) {
       throw new Error(`Failed to process contact request: HTTP ${resStatus} - ${res}`)
     }
-    $("#contact-modal").modal('toggle')
-    triggerStatusModal("Request submitted", "For anything else please contact Andrew directly at ")
+    // $("#contact-modal").modal('toggle')
+    // triggerStatusModal("Request submitted", "For anything else please contact Andrew directly at ")
   }).catch(function(error) {
-    $("#contact-modal").modal('toggle')
+    // $("#contact-modal").modal('toggle')
     triggerStatusModal("Something went wrong :(", "Please contact Andrew directly at ")
     console.error(error)
   })
 }
 function triggerStatusModal(label, text) {
-  $('#status-modal-label').text(label)
-  $('#status-modal-text').text(text)
-  $('#status-modal').modal('toggle')
+  // $('#status-modal-label').text(label)
+  // $('#status-modal-text').text(text)
+  // $('#status-modal').modal('toggle')
+  console.log(`${label}: ${text}`)
 }
